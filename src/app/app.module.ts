d89180d38badb180e6f341df55715d2fc5b19d53
Victor B <victor@semanticwebindia.com>
2023-06-07 11:20:12 +0530

    bolierpalte code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-//REVIEW -  Import the Forms module
-import { FormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,7 +12,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
