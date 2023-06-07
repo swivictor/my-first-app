78164603ae7a20d1220ecb476e600a4cff00461b
Victor B <victor@semanticwebindia.com>
2023-06-07 15:08:30 +0530

    Compenent Created

--- Notes.md
+++ Notes.md
@@ -2,3 +2,10 @@
 ```{cmd}
 ng add @ng-bootstrap/ng-bootstrap --project my-first-app
 ```
+### Adding New Components:
+```{cmd}
+ng generate component <component-name> --skip-test
+ng g c recipies --skip-tests
+ng g c recipies/recipe-list --skip-tests
+ng g c recipies/recipe-details --skip-tests 
+```
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+// Header Component
 import { HeaderComponent } from './header/header.component';
+// Recipes Component
+import { RecipiesComponent } from './recipies/recipies.component';
+import { RecipeListComponent } from './recipies/recipe-list/recipe-list.component';
+import { RecipeDetailsComponent } from './recipies/recipe-details/recipe-details.component';
+import { RecipeItemsComponent } from './recipies/recipe-list/recipe-items/recipe-items.component';
+// Shopping List Component
+import { ShopingListComponent } from './shoping-list/shoping-list.component';
+import { ShopingEditComponent } from './shoping-list/shoping-edit/shoping-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    RecipiesComponent,
+    RecipeListComponent,
+    RecipeDetailsComponent,
+    RecipeItemsComponent,
+    ShopingListComponent,
+    ShopingEditComponent
   ],
   imports: [
     BrowserModule,
