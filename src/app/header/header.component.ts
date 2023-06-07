b8a57c4785247c0851661333c79d7dd6d347d380
Victor B <victor@semanticwebindia.com>
2023-06-07 13:55:29 +0530

    Component Added manually - HeaderComponent

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+// NOTE - Component is Decorater
+@Component({
+    selector: 'app-header', //NOTE -  Unique selector to avoid collisions
+    templateUrl: './header.component.html'
+})
+export class HeaderComponent {
+
+}
