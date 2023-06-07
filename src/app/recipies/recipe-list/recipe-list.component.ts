9a9eaea129f7f699bf79302ef01eb6d51328b6fb
Victor B <victor@semanticwebindia.com>
2023-06-07 16:59:16 +0530

    Recipe List Implementation with ngFor

--- src/app/recipies/recipe-list/recipe-list.component.ts
+++ src/app/recipies/recipe-list/recipe-list.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipies.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    // NOTE - Create a new dummay Recipe with Recipe Model
+    new Recipe(
+      'A Test Recipe',
+      'This is a Test Recipe',
+      'https://www.livecrafteat.com/wp-content/uploads/2016/01/salad-ingredients.jpg'
+    ),
+  ];
 }
--- src/app/recipies/recipies.model.ts
+++ src/app/recipies/recipies.model.ts
@@ -1,3 +1,13 @@
+// NOTE - Recipe Moldel Class
+// The model represents a specific car model, such as a "Sports Car" or "SUV." 
+// Each car model has its own set of properties, methods, and behavior.
+// The model defines the blueprint for creating instances of cars with specific 
+// characteristics and features. It encapsulates the state (properties) and behavior 
+// (methods) of the car.
+// Just like a car model specifies its properties (color, engine type, seating capacity) 
+// and behavior (accelerate, brake, turn), a model in Angular defines the properties
+//  and behavior of a specific data entity.
+
 export class Recipe {
     public name: string;
     public description: string;
