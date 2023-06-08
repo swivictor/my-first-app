60f51dba97a41c74baf7931c28626a351f3df341
Victor B <victor@semanticwebindia.com>
2023-06-08 12:22:06 +0530

    05 -  Data Binding Start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -11,8 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
